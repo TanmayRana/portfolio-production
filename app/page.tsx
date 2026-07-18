d82b6f5aa9ec2dc06266a2114dacf2f68d8566cf
TanmayRana <tanmayrana2001@gmail.com>
2026-07-18 07:39:25 +0530

    Testing production..

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import HeroContainer from '@/containers/home/HeroContainer'
-import AboutContainer from '@/containers/home/AboutContainer'
-import Skills from '@/components/sections/Skills'
-import MyExpertise from '@/components/sections/MyExpertise'
-import FeaturedProjects from '@/components/sections/FeaturedProjects'
-import WorkExperience from '@/components/sections/WorkExperience'
-import ProfessionalCredentials from '@/components/sections/ProfessionalCredentials'
-import Contact from '@/components/sections/Contact'
+import HeroContainer from "@/containers/home/HeroContainer";
+import AboutContainer from "@/containers/home/AboutContainer";
+import Skills from "@/components/sections/Skills";
+import MyExpertise from "@/components/sections/MyExpertise";
+import FeaturedProjects from "@/components/sections/FeaturedProjects";
+import WorkExperience from "@/components/sections/WorkExperience";
+import ProfessionalCredentials from "@/components/sections/ProfessionalCredentials";
+import Contact from "@/components/sections/Contact";
 
-import FloatingNavbar from '@/components/ui/FloatingNavbar'
+import FloatingNavbar from "@/components/ui/FloatingNavbar";
 
 const page = () => {
+  console.log("Testing production......");
+
   return (
     <main className="h-screen w-full overflow-y-scroll overflow-x-hidden snap-y snap-mandatory scroll-smooth relative">
       <FloatingNavbar />
@@ -22,7 +24,7 @@ const page = () => {
       <ProfessionalCredentials />
       <Contact />
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
